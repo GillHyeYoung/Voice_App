166f524ac0c8c3d12945e861a5a23d0eb799515a
gill <haee02@naver.com>
2018-03-27 18:44:08 +0900

    not storage2

--- app/src/main/java/com/example/root/voice_app/MainActivity.java
+++ app/src/main/java/com/example/root/voice_app/MainActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
